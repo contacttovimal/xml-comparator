dca28f53ae5895b4c073d3202abc2bd9b1efe8c4
vimal <contacttovimal@gamail.com>
2022-10-21 20:09:46 +0800

    base setup

---
+++ src/test/java/com/xml/comparator/TradeEngineTest.java
@@ -0,0 +1,20 @@
+package com.xml.comparator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TradeEngineTest {
+
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void checkSellOrders() throws Exception {
+
+    }
+
+
+}
